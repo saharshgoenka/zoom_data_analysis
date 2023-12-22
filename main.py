dd72e59b9bacac25d212d7ff9e13985f86347a8f
saharshgoenka <saharsh.goenka3@gmail.com>
2023-12-22 10:23:28 -0800

    Update: Create 3 graphs for Scope, View Permission, and Manage Permission

--- main.py
+++ main.py
@@ -2,30 +2,52 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import json
 
-# Read JSON data from the "items.json" file
 with open('zoom_marketplace_2023-11-21.json', 'r') as file:
     data = json.load(file)
 
-# Create a DataFrame from the JSON data
 df = pd.DataFrame(data)
 
-# Create a list of all permissions required by apps
-all_permissions = [scope for scopes in df['scopes'] for scope in scopes]
+scope_data = [scope for scopes in df['scopes'] for scope in scopes]
+viewPermission_data = [wow for wows in df['viewPermissions'] for wow in wows]
+managePermission_data = [wow for wows in df['managePermissions'] for wow in wows]
 
-# Count the occurrences of each permission
-permission_counts = pd.Series(all_permissions).value_counts()
+permission_counts = pd.Series(scope_data).value_counts()
+test1 = pd.Series(viewPermission_data).value_counts()
+test2 = pd.Series(managePermission_data).value_counts()
 
-# Plot the graph with adjusted figure size and spacing
-plt.figure(figsize=(18, 8))  # Adjust the figure size as needed
-permission_counts.plot(kind='bar', width=0.8, color='green')  # Adjust width and color as needed
+plt.figure(figsize=(18, 8))
+permission_counts.plot(kind='bar', width=0.8, color='green')
 plt.title('Number of Apps Requiring Each Type of Scope')
-plt.xlabel('Permission')
+plt.xlabel('Scope Permission')
 plt.ylabel('Number of Apps')
-plt.xticks(rotation=45, ha='right')  # Adjust rotation angle and alignment as needed
-plt.tight_layout()  # Adjust layout for better spacing
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
+
+test1.plot(kind='bar', width=0.8, color='blue')
+plt.title('Number of Apps Requiring Each Type of View Permission')
+plt.xlabel('View Permission')
+plt.ylabel('Number of Apps')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
+
+test2.plot(kind='bar', width=0.8, color='red')
+plt.title('Number of Apps Requiring Each Type of Manage Permission')
+plt.xlabel('Manage Permission')
+plt.ylabel('Number of Apps')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
 plt.show()
 
 
-# Print the results in the terminal
-print("Permission Counts:")
+print("Scope Permission Counts:")
 print(permission_counts)
+
+print()
+print("View Permission Counts:")
+print(test1)
+
+print()
+print("Manage Permission Counts:")
+print(test2)
